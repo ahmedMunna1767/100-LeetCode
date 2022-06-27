8107c8ad2665531622a9e8d7001196517fc921a8
Ahmed Munna <ahmedmunna172367@gmail.com>
2022-06-27 17:55:27 +0600

    Regular Expression Matching

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 1. Median of two Sorted array
    1. Serial 4
    2. Difficulty Hard
+2. Regular Expression Matching
+   1. Serial 10
+   2. Difficulty Hard
+   3. Recursive Solution
+      1. Time Complexity $O(T+P)2^{(T+P/2)}$
+      2. Space Complexity $O(T^2 + P^2)$
+   4. Dynamic Programming
+      1. Time Complexity $O(T*P)$
+      2. Space Complexity $O(T*P)$
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const (
+	NOTVISITED = iota
+	TRUE
+	FALSE
+)
+
+// Problem One
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
 	nums1Index := 0
@@ -33,8 +42,73 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	}
 }
 
-func main() {
-	fmt.Println("Hi.....")
+// Problem Two
+// * Recursive Solution * //
+func isMatch(s string, p string) bool {
+	if p == "" {
+		return s == ""
+	}
+	// check if first character matches
+	firstMatch := (!(s == "") && (p[0] == s[0] || p[0] == '.'))
 
+	if len(p) >= 2 && p[1] == '*' {
+		// if there is a klene star
+		return isMatch(s, p[2:]) || (firstMatch && isMatch(s[1:], p))
+	} else {
+		// If 2nd character is not a klene start
+		return firstMatch && (isMatch(s[1:], p[1:]))
+	}
+}
+
+func isMatchDynamic(s string, p string) bool {
+
+	// Init the memoization array
+	memo := make([][]int, len(s)+1)
+	for i := 0; i < len(s)+1; i++ {
+		memo[i] = make([]int, len(p)+1)
+	}
+
+	for i := 0; i < len(s)+1; i++ {
+		for j := 0; j < len(p)+1; j++ {
+			memo[i][j] = NOTVISITED
+		}
+	}
+
+	var dp func(i, j int) bool
+	dp = func(i, j int) bool {
+		// if the node is previously computed, return appropriate value
+		if memo[i][j] != NOTVISITED {
+			return TRUE == memo[i][j]
+		}
+
+		var ans bool
+		if j == len(p) {
+			// reach both end at the same time
+			ans = (i == len(s))
+		} else {
+			// if the first characters match
+			firstMatch := (i < len(s) && (p[j] == s[i] || p[j] == '.'))
+
+			// in case of a kleene star
+			if j+1 < len(p) && p[j+1] == '*' {
+				ans = (dp(i, j+2) || firstMatch && dp(i+1, j))
+			} else {
+				ans = firstMatch && dp(i+1, j+1)
+			}
+		}
+		if ans {
+			memo[i][j] = TRUE
+		} else {
+			memo[i][j] = FALSE
+		}
+		return ans
+	}
+
+	return dp(0, 0)
+}
+
+func main() {
+	fmt.Println(isMatchDynamic("aa", ".."))
+	fmt.Println(isMatch("aa", ".."))
 	fmt.Println(findMedianSortedArrays([]int{1, 2}, []int{3, 4}))
 }
