0c6d2d6642030f9dd4123db7fede02222ba35ce9
ahmedMunna1767 <ahmedmunna172367@gmail.com>
2022-07-19 12:16:49 +0600

    Updated TimeBased Key Value pair Problem

--- neetcode/binarysearch/timebasedKeyValue.go
+++ neetcode/binarysearch/timebasedKeyValue.go
@@ -8,32 +8,33 @@ type valStruct struct {
 }
 
 type TimeMap struct {
-	Value   map[string][]valStruct
-	PrevVal map[string]int
+	Value map[string][]valStruct
+	/* PrevVal map[string]int */
 }
 
 func Constructor() TimeMap {
 	val := make(map[string][]valStruct)
-	prevVal := make(map[string]int)
-	return TimeMap{Value: val, PrevVal: prevVal}
+	return TimeMap{Value: val}
+	/* prevVal := make(map[string]int)
+	return TimeMap{Value: val, PrevVal: prevVal} */
 }
 
-func (this *TimeMap) Set(key string, value string, timestamp int) {
-	if _, ok := this.Value[key]; ok {
-		this.PrevVal[key] = timestamp
-	}
-	this.Value[key] = append(this.Value[key], valStruct{value: value, timestamp: timestamp})
-	fmt.Printf("%v\n", this.Value)
+func (tm *TimeMap) Set(key string, value string, timestamp int) {
+	/* if _, ok := tm.Value[key]; ok {
+		tm.PrevVal[key] = timestamp
+	} */
+	tm.Value[key] = append(tm.Value[key], valStruct{value: value, timestamp: timestamp})
+	fmt.Printf("%v\n", tm.Value)
 }
 
-func (this *TimeMap) Get(key string, timestamp int) string {
-	if timestamp < this.PrevVal[key] {
+/* func (tm *TimeMap) Get(key string, timestamp int) string {
+	if timestamp < tm.PrevVal[key] {
 		return ""
 	}
 
 	var largestPrev string
 	largestPrev = ""
-	for _, v := range this.Value[key] {
+	for _, v := range tm.Value[key] {
 		if v.timestamp == timestamp {
 			largestPrev = v.value
 			break
@@ -43,4 +44,26 @@ func (this *TimeMap) Get(key string, timestamp int) string {
 		largestPrev = v.value
 	}
 	return largestPrev
+}
+*/
+
+// Binary Search Solution
+func (tm *TimeMap) Get(key string, timestamp int) string {
+	if _, ok := tm.Value[key]; !ok {
+		return ""
+	}
+	values := tm.Value[key]
+	left, right := 0, len(values)-1
+	res := ""
+	for left <= right {
+		mid := (left + right) / 2
+		if values[mid].timestamp <= timestamp {
+			res = values[mid].value
+			left = mid + 1
+
+		} else {
+			right = mid - 1
+		}
+	}
+	return res
 }
---
+++ neetcode/slidingwindow/permutationInString.go
@@ -0,0 +1,11 @@
+package slidingwindow
+
+import "fmt"
+
+func checkInclusion(s1 string, s2 string) bool {
+	return false
+}
+
+func CheckInclusion() {
+	fmt.Println(checkInclusion("", ""))
+}
